9d225795e6a341af21321ecc830dd93e06adb7dd
svang01 <shattv@yahoo.com>
2023-05-09 00:27:59 -0400

    initial

---
+++ src/Casting.java
@@ -0,0 +1,15 @@
+public class Casting {
+    public static void main(String[] args) {
+        double num = 112.35;
+        int data = (int)num;
+
+        System.out.println("Double: " + num);
+        System.out.println("int: " + data);
+
+        String value = "49";
+        int number = Integer.parseInt(value);
+
+        System.out.println("The string value is: " + value);
+        System.out.println("The integer value is: " + number);
+    }
+}
